8a8a1d31947528c429370abfb1c0047b4d4a52df
Joseph Chacko <joseph.chacko@resilinc.com>
2018-05-03 19:30:40 -0700

    added pickle files

---
+++ eda.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 12:27:09 2018
+
+@author: JosephChacko
+"""
+
+from bs4 import BeautifulSoup
+import urllib3
+import json
+import pandas as pd
+import os
+import re
+from time import sleep
+import pickle
+
+os.chdir('C:\\Users\\JosephChacko\\Documents\\Personal\\chicago')
+
+http=urllib3.PoolManager()
+
+url='http://www.skyscrapercenter.com/compare-data/submit?base_city=1539&base_company=All&base_height_range=0&base_max_year=9999&base_min_year=1900&output%5B%5D=list&skip_comparison=on&status%5B%5D=COM&status%5B%5D=PRO&status%5B%5D=STO&status%5B%5D=UC&status%5B%5D=UCT&type%5B%5D=building'
+response=http.request('GET',url)
+content=urllib3.urlopen(url).read()
+soup=BeautifulSoup(response.data)
+scripts=soup.findAll('script', type='text/javascript')
+
+
+x=scripts[2].text
+y=x[x.find('['):x.rfind(']')+1]
+X=json.loads(x[x.find('['):x.rfind(']')+1])
+df=pd.DataFrame.from_dict(X)
+
+with open('chicago_skyscrapers.txt','w') as f:
+    f.write(y)
+    
+df.to_excel('chicago_buildings.xlsx')
+
+
+details=[]
+for val in df['url']:
+    url='http://www.skyscrapercenter.com'+val
+    print(url)
+    response=http.request('GET',url)
+    soup=BeautifulSoup(response.data)
+    tables=soup.findAll('table',{"class":"table table-condensed table-building-data"})
+    companies=[val]
+    for row in tables[len(tables)-1].findAll('tr'):
+        for datum in row.findAll('td'):
+            companies.append(re.sub(r'\t|\n|\r','',datum.text.strip()))
+    print(companies)
+    details.append(companies)
+    sleep(1)
+
+df['details']=details
+
+x=0
+Architects=[]
+for val in details:
+#    print (val)
+    try:
+        i=val.index('Architect')
+#        print (val[i+1],' : ', val[i+2])
+        Architects.append(val[i+2])
+    except ValueError:
+        Architects.append('N/A')
+
+
+df['Architect']=Architects
+    
+df2=df.convert_objects(convert_numeric=True)
+
+df2.to_pickle('df2.pck')
+df.to_pickle('df.pck')
